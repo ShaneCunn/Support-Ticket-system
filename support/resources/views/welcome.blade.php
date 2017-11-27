a1c62e23a901ec2255699b41c782ef9a0002f46f
Shane Cunningham <mayhem2277@gmail.com>
2017-11-27 08:44:18 +0000

    update name of app and added support link

--- support/resources/views/welcome.blade.php
+++ support/resources/views/welcome.blade.php
@@ -83,6 +83,7 @@
                 </div>
 
                 <div class="links">
+                    <a href="/home">Support</a>
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
