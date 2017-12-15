03d28b12dac9d4d2d031a547d35d2465707475fd
Shane Cunningham <shanecunningham@live.ie>
2017-12-15 11:04:21 +0000

    added alias and class intervention  to app.php

--- support/config/app.php
+++ support/config/app.php
@@ -183,6 +183,9 @@ return [
         Collective\Html\HtmlServiceProvider::class,
 
         // ...
+
+        // ...  intervention image package
+        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -233,6 +236,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
 
     ],
