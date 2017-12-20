9bc849244b760c508df37690da63ff72e5ec7a05
Shane Cunningham <shanecunningham@live.ie>
2017-12-20 10:30:19 +0000

    resized editor & text field to fit page

--- support/resources/views/tickets/create.blade.php
+++ support/resources/views/tickets/create.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-9 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Open New Ticket</div>
 
@@ -17,9 +17,9 @@
                         {!! csrf_field() !!}
 
                         <div class="form-group{{ $errors->has('title') ? ' has-error' : '' }}">
-                            <label for="title" class="col-md-4 control-label">Title</label>
+                            <label for="title" class="col-md-2 control-label">Title</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <input id="title" type="text" class="form-control" name="title"
                                        value="{{ old('title') }}">
 
@@ -32,9 +32,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }}">
-                            <label for="category" class="col-md-4 control-label">Category</label>
+                            <label for="category" class="col-md-2 control-label">Category</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <select id="category" type="category" class="form-control" name="category">
                                     <option value="">Select Category</option>
                                     @foreach ($categories as $category)
@@ -51,9 +51,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('priority') ? ' has-error' : '' }}">
-                            <label for="priority" class="col-md-4 control-label">Priority</label>
+                            <label for="priority" class="col-md-2 control-label">Priority</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <select id="priority" type="" class="form-control" name="priority">
                                     <option value="">Select Priority</option>
                                     <option value="low">Low</option>
@@ -70,9 +70,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('message') ? ' has-error' : '' }}">
-                            <label for="message" class="col-md-4 control-label">Message</label>
+                            <label for="message" class="col-md-2 control-label">Message</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <textarea rows="10" id="message" class="form-control" name="message"></textarea>
 
                                 @if ($errors->has('message'))
@@ -80,12 +80,12 @@
                                         <strong>{{ $errors->first('message') }}</strong>
                                     </span>
                                 @endif
-                                test  hello
+
                             </div>
                         </div>
 
                         <div class="form-group {{ $errors->has('support_image') ? ' has-error' : '' }}">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-9 col-md-offset-2">
                                 {{Form::label('support_image', 'Upload support image:')}}
                                 {{Form::file('support_image')}}
                                 <span class="help-block">
@@ -96,7 +96,7 @@
 
                         </div>
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
+                            <div class="col-md-9 col-md-offset-2">
                                 <button type="submit" class="btn btn-primary">
                                     <i class="fa fa-btn fa-ticket"></i> Open Ticket
                                 </button>
