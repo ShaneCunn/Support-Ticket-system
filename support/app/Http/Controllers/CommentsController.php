f17fc7836a42f659afeff41caa572f1f20cf91a4
Shane Cunningham <shanecunningham@live.ie>
2017-12-20 14:31:30 +0000

    added comments to comment controller

--- support/app/Http/Controllers/CommentsController.php
+++ support/app/Http/Controllers/CommentsController.php
@@ -17,23 +17,27 @@ class CommentsController extends Controller
 
     public function postComment(Request $request, AppMailer $mailer)
     {
+
+        // validated request variable/array  to see if field are
         $this->validate($request, [
             'comment' => 'required',
-            'comment_image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+            'comment_image' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048' // check file type and file size
 
         ]);
 
 
-        $commentimage = new Comment();
+        $commentimage = new Comment(); // comment object to variable to be checked
 
 
-        if ($request->hasFile('comment_image')) {
+        if ($request->hasFile('comment_image')) { //  check to see if there is a  file in the request array
 
             $image = $request->file('comment_image');
             $filename = time() . '.' . $image->getClientOriginalExtension();
             $location = public_path('images/comments/' . $filename);
-            Image::make($image)->fit(800)->save($location);
+            Image::make($image)->fit(800)->save($location); // save file to location using intervention library
             $commentimage->image = $filename;
+
+            // insert a new record in the database , passing in the following variables
             $comment = Comment::create([
                 'ticket_id' => $request->input('ticket_id'),
                 'user_id' => Auth::user()->id,
@@ -44,7 +48,7 @@ class CommentsController extends Controller
 
 
         } else {
-
+            // if no image is uploaded then  insert these fields
             $comment = Comment::create([
                 'ticket_id' => $request->input('ticket_id'),
                 'user_id' => Auth::user()->id,
