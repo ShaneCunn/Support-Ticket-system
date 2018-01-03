219b69b7a53cc2cc5e7556fa3300b53e80de431a
Shane Cunningham <shanecunningham@live.ie>
2018-01-03 13:26:55 +0000

    added in basic view of total tickets and open and closed

--- support/resources/views/home.blade.php
+++ support/resources/views/home.blade.php
@@ -7,7 +7,26 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">Dashboard
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#">Home</a></li>
+
+
+                            @if (Auth::user()->is_admin)
+                                <li>
+                                    <a href="{{ url('admin/tickets') }}"> See all
+                                        tickets</a>
+                                </li>
+                            @else
+                                <li>
+                                    <a href="{{ url('my_tickets') }}">See all your
+                                        tickets</a></li>
+                                <li><a href="{{ url('new_ticket') }}">Open new ticket</a>
+                                </li>
+
+                            @endif
+                        </ul>
+                    </div>
 
                     <div class="panel-body">
                         <!-- CSRF Token -->
@@ -18,26 +37,65 @@
 
                         <!-- Styles -->
                         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+                        <div class="panel-heading"><a href="{{ url('/') }}">
 
+                                {{ config('app.name', 'Support ticket app') }}
+                            </a></div>
+                        <div class="panel-body">
 
-                        <!-- Branding Image -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <!-- Branding Image -->
 
-                            {{ config('app.name', 'Support ticket app') }}
-                        </a>
-                        <p>You are logged in!</p>
-
+                            <p>You are logged in!</p>
+                          
+                        </div>
                         @if (Auth::user()->is_admin)
-                            <p>
-                                <a href="{{ url('admin/tickets') }}" class="btn btn-primary" role="button"> See all
-                                    tickets</a>
-                            </p>
-                        @else
-                            <p>
-                                <a href="{{ url('my_tickets') }}" class="btn btn-primary" role="button">See all your
-                                    tickets</a>
-                                <a href="{{ url('new_ticket') }}" class="btn btn-success" role="button">Open new ticket</a>
-                            </p>
+                            <div class="row">
+                                <div class="col-lg-3 col-md-4 col-lg-offset-1">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <div class="row">
+                                                <div class="col-xs-3" style="font-size: 5em;">
+                                                    <i class="glyphicon glyphicon-th"></i>
+                                                </div>
+                                                <div class="col-xs-9 text-right">
+                                                    <h1>{{ \App\Ticket::count() }}</h1>
+                                                    <div>Total tickets</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="panel panel-danger">
+                                        <div class="panel-heading">
+                                            <div class="row">
+                                                <div class="col-xs-3" style="font-size: 5em;">
+                                                    <i class="glyphicon glyphicon-wrench"></i>
+                                                </div>
+                                                <div class="col-xs-9 text-right">
+                                                    <h1>{{ \App\Ticket::where('status','=','Open')->count() }}</h1>
+                                                    <div>Open tickets</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-3 col-md-4">
+                                    <div class="panel panel-success">
+                                        <div class="panel-heading">
+                                            <div class="row">
+                                                <div class="col-xs-3" style="font-size: 5em;">
+                                                    <i class="glyphicon glyphicon-thumbs-up"></i>
+                                                </div>
+                                                <div class="col-xs-9 text-right">
+                                                    <h1>{{ \App\Ticket::where('status','=','Closed')->count() }}</h1>
+                                                    <span>Closed tickets</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         @endif
                     </div>
                 </div>
