7cf0d11fb8fb04054321d2ea154f2dbfee57e2e8
Shane Cunningham <shanecunningham@live.ie>
2018-01-16 13:51:16 +0000

    updated alerts to be closeable and added failed updated

--- support/resources/views/includes/flash.blade.php
+++ support/resources/views/includes/flash.blade.php
@@ -1,5 +1,11 @@
 @if (session('status'))
-    <div class="alert alert-success">
+    <div class="alert alert-success alert-dismissable">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
         {!!  session('status') !!}
+    </div>
+@elseif (session('failed'))
+    <div class="alert alert-danger alert-dismissable">
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+        {!!  session('failed') !!}
     </div>
 @endif
