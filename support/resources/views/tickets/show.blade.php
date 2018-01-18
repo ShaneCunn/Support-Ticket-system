002b0d4eecf6781cbb9fffb75048084f61a59823
Shane Cunningham <mayhem2277@gmail.com>
2018-01-18 11:04:27 +0000

    fixed the view of show tickets to be fully repsonsive

--- support/resources/views/tickets/show.blade.php
+++ support/resources/views/tickets/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <!-- TODO: Make rows sortable -->.
-    <div class="row">
+   
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">
@@ -168,5 +168,5 @@
                 </div>
             </div>
         </div>
-    </div>
+
 @endsection
